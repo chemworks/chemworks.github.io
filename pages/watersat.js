9e588c88bee199f3bf8bad9adbd7441539fe12a9
ChemWorks <ftodeschini@gmail.com>
2022-10-07 20:33:50 -0300

    add some changes

--- pages/watersat.js
+++ pages/watersat.js
@@ -1,7 +1,28 @@
+function run() {
+  let d = new Date();
 
-function run(){
-
-let d = new Date();
-
-document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+  document.body.innerHTML = " <h1>Today's date is " + d + " </h1>";
 }
+
+const btncalc = document.querySelector("#btncalc");
+btncalc.addEventListener("click", () => {
+  alert("dsadssad");
+});
+
+const btnclear = document.querySelector("#btnclear");
+btnclear.addEventListener("click", (e) => {
+  /*run();*/
+  e.target.style.background = "blue";
+  console.log(e.target);
+});
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    alert(button.id);
+  });
+});
