829476934d126c35d2cba9f16005e18db9085dfc
ChemWorks <ftodeschini@gmail.com>
2022-10-08 09:16:01 -0300

    Add functions and arrangin html

--- pages/watersat.js
+++ pages/watersat.js
@@ -1,28 +1,78 @@
 function run() {
+  // Function to call date
   let d = new Date();
 
   document.body.innerHTML = " <h1>Today's date is " + d + " </h1>";
 }
 
+// for each in input
+const inputs = document.querySelectorAll("input");
+
 const btncalc = document.querySelector("#btncalc");
 btncalc.addEventListener("click", () => {
-  alert("dsadssad");
+  //inputs.forEach((inp) => {
+  //alert(inp.value);
+  //alert(inp.id);
+  //});
+
+  const GasSG = Number(document.getElementById("GasSG").value); // grab the value and convert to number
+  const GasTemp = Number(document.getElementById("GasTemp").value);
+  const GasPres = Number(document.getElementById("GasPres").value);
+  const resPara = document.getElementById("result");
+  let WC = water_content(GasPres, GasTemp);
+    // changing the result para to the result
+  resPara.innerHTML=WC;
 });
 
 const btnclear = document.querySelector("#btnclear");
 btnclear.addEventListener("click", (e) => {
-  /*run();*/
   e.target.style.background = "blue";
   console.log(e.target);
+  const resPara = document.getElementById("result");
+    resPara.innerHTML="";
 });
 
-// buttons is a node list. It looks and acts much like an array.
-const buttons = document.querySelectorAll("button");
+function water_content(pres, temp) {
+  /*
+    Return the Water of a gas, temperature range
+    35 70 C, for other ranges see link
+    Parameters
+    ----------
+    Temp : float
+        Temperature of the gas in C
+    Pressure : float
+        Pressure Of gas in kgf/cm2g
+    Returns
+    -------
+    float
+        Water content in kg / MMSCM (10^6 Sm3)
 
-// we use the .forEach method to iterate through each button
-buttons.forEach((button) => {
-  // and for each one we add a 'click' listener
-  button.addEventListener("click", () => {
-    alert(button.id);
-  });
-});
+    References
+    ----------
+    Ref: https://www.jmcampbell.com/tip-of-the-month/2014/09/lean-sweet-natural-gas-water-content-correlation/
+
+   */
+
+  pres = pres * 0.96784111;
+  let coefA;
+  let coefB;
+  let coefC;
+  let A;
+  let B;
+  if (temp < 35) {
+    coefA = [1011.528, 2.392205];
+    coefB = [2.392205, 153.4538];
+    coefC = [46.04953, 53.93539];
+  } else if (temp < 70) {
+    coefA = [8012.09, 5.148325];
+    coefB = [229.152, 186.1237];
+    coefC = [60.06752, 59.92855];
+  } else if (temp < 100) {
+    coefA = [8101.695, 1.56];
+    coefB = [232.5031, 108.4];
+    coefC = [61.19779, 33.03525];
+  }
+  A = coefA[0] * Math.exp((-1 * (temp - coefB[0]) ** 2) / (2 * coefC[0] ** 2));
+  B = coefA[1] * Math.exp((-1 * (temp - coefB[1]) ** 2) / (2 * coefC[1] ** 2));
+  return (A / pres + B) * 1000;
+}
