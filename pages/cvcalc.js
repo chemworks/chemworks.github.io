071f66444060f4c375b6dffd022ac66a3b8b5b29
ChemWorks <ftodeschini@gmail.com>
2022-10-09 08:34:34 -0300

    Functions and forms

---
+++ pages/cvcalc.js
@@ -0,0 +1,11 @@
+function convert_CtoRe(c) {
+  return (c + 273.15) * (9 / 5);
+}
+
+function convert_kgftopsi(P) {
+  return P * 14.223;
+}
+
+function convert_m3tocuft(F) {
+  return F * 35.314667;
+}
